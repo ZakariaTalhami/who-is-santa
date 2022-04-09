28670ae0dd670c06189a5c0fc6935943094b213d
ZakariaTalhami <zakaria.a.a.talhami@gmail.com>
2022-04-09 15:56:05 +0300

    feat: add body validation to register api

--- backend/src/app/api/user/register.ts
+++ backend/src/app/api/user/register.ts
@@ -1,11 +1,30 @@
 import { Request, Response, Router } from "express";
+import { body } from "express-validator";
 import { IUserDto } from "../../interface/user";
+import { validateRequest } from "../../middleware/request-validator";
 import { UserService } from "../../services";
 
 const router = Router();
 
 router.post(
   "/register",
+  [
+    body("email").isEmail().withMessage("email field must be a valid email"),
+    body("username")
+      .isString()
+      .notEmpty()
+      .withMessage("username field must be defined"),
+    body("password")
+      .isString()
+      .notEmpty()
+      .withMessage("password field must be defined")
+      .isLength({
+        min: 8,
+      })
+      // TODO: add password pattern check
+      .withMessage("password field must have at least 8 characters"),
+  ],
+  validateRequest,
   async (req: Request<any, any, IUserDto>, res: Response) => {
     const userDTO: IUserDto = req.body;
 
